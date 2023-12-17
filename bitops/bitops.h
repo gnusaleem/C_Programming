b79bbdd639b7687d8b64b9ed9e3eaf1e6612a63f
Saleemuddin Shaik <saleemuddinshaik.s@zilogic.com>
2023-12-17 14:16:23 +0530

    Added set_clear bit
    Signed-off-by: Saleemuddin Shaik <saleemuddinshaik.s@zilogic.com>

--- bitops/bitops.h
+++ bitops/bitops.h
@@ -7,3 +7,4 @@ enum {
 
 #define set(data, pos)		data |= (1 << pos)
 #define clear(data, pos)	data &= ~(1 << pos)
+#define set_clear(set_data, clear_data, val)	val = (val | (1 << set_data)) &~(1 << clear_data)
--- bitops/main.c
+++ bitops/main.c
@@ -3,23 +3,37 @@
 
 int main(void)
 {
-	int num;
-	int pos;
+	int num1;
+	int pos1;
 	int opt;
+	int pos2;
 
 	printf("Choose the option\n0.set\t1.clear\t2.setclear\t3.toggle\n");
 	scanf("%d", &opt);
-	printf("Enter the number\n");
-	scanf("%d", &num);
-	printf("Enter the position\n");
-	scanf("%d", &pos);
 
 	switch(opt) {
 	case SET:
-		printf("set bit is %d\n", set(num, pos));
+		printf("Enter the number\n");
+		scanf("%d", &num1);
+		printf("Enter the position\n");
+		scanf("%d", &pos1);
+		printf("set bit is %d\n", set(num1, pos1));
 		break;
 	case CLEAR:
-		printf("cleat bit is %d\n", clear(num, pos));
+		printf("Enter the number\n");
+		scanf("%d", &num1);
+		printf("Enter the position\n");
+		scanf("%d", &pos1);
+		printf("cleat bit is %d\n", clear(num1, pos1));
+		break;
+	case SETCLEAR:
+		printf("Enter the number\n");
+		scanf("%d", &num1);
+		printf("Enter the 1st  number to set\n");
+		scanf("%d", &pos1);
+		printf("Enter the 2nd position to clear\n");
+		scanf("%d", &pos2);
+		printf("setcleat bit is %d\n", set_clear(pos1, pos2, num1));
 		break;
 	default:
 		printf("invalid option\n");
