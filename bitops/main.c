5575ea2c683882f2afd609b879be6e8fed837376
Saleemuddin Shaik <saleemuddinshaik.s@zilogic.com>
2024-08-31 09:07:24 +0530

    Added Toggle and checkbit programs
    Signed-off-by: Saleemuddin Shaik <saleemuddinshaik.s@zilogic.com>

--- bitops/main.c
+++ bitops/main.c
@@ -6,9 +6,11 @@ int main(void)
 	int num1;
 	int pos1;
 	int opt;
+	int len;
 	int pos2;
 
 	printf("Choose the option\n0.set\t1.clear\t2.setclear\t3.toggle\n");
+	printf("4.togglerang\t5.checkbit\n");
 	scanf("%d", &opt);
 
 	switch(opt) {
@@ -34,6 +36,29 @@ int main(void)
 		printf("Enter the 2nd position to clear\n");
 		scanf("%d", &pos2);
 		printf("setcleat bit is %d\n", set_clear(pos1, pos2, num1));
+		break;
+	case TOGGLE:
+		printf("Enter the number\n");
+		scanf("%d", &num1);
+		printf("Enter the position\n");
+		scanf("%d", &pos1);
+		printf("toggle bit is %d\n", toggle(num1, pos1));
+		break;
+	case TOGGLERANGE:
+		printf("Enter the number\n");
+		scanf("%d", &num1);
+		printf("Enter the position\n");
+		scanf("%d", &pos1);
+		printf("Enter the range\n");
+		scanf("%d", &len);
+		printf("toggle bit is %d\n", togglerange(num1, pos1, len));
+		break;
+	case CHECKBIT:
+		printf("Enter the number\n");
+		scanf("%d", &num1);
+		printf("Enter the position\n");
+		scanf("%d", &pos1);
+		printf("Checkbit is %d\n", checkbit(num1, pos1));
 		break;
 	default:
 		printf("invalid option\n");
