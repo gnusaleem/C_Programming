ae03c91ebee4e583dcfdd374adfd713cb63e9940
Saleemuddin Shaik <saleemuddin.shaik8@gmail.com>
2024-09-20 23:51:38 +0530

    Adding New branch for Alogirthms
    Added Linear_search Alogirthm
    
    Signed-off-by: Saleemuddin Shaik <saleemuddin.shaik8@gmail.com>

---
+++ Algorithms/linear_search.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *arr, int size, int val) {
+	int i, pos=0;
+
+	if (size == 0)
+		return -1;
+
+	for (i = 0; i < size; i++) {
+		if (arr[i] == val) {
+			pos = i;
+			return pos;
+		}
+	}
+	return -1;
+}
+
+int main(void)
+{
+	int i, res;
+	int *ptr, size;
+	int val = 0;
+	
+	printf("Enter the Array size\n");
+	scanf("%d", &size);
+
+	ptr = (int *)malloc(sizeof(int) * size);
+
+	if (ptr == NULL) {
+		printf("Memory not allocated\n");
+		free(ptr);
+	} else {
+		printf("Enter the  %d Number to be stored in array\n", size);
+		for (i = 0; i <= size; i++) {
+			scanf("%d", &ptr[i]);
+		}
+
+		for (i = 0; i <= size; i++) {
+			printf("pos = %d, value = %d, address is %p\n", i, ptr[i], (int *)&ptr[i]);
+		}
+
+		printf("Enter the value to be searched in array\n");
+		scanf("%d", &val);
+
+		res = linear_search(ptr, size, val);
+
+		if (res == -1)
+			printf("Element not found\n");
+		else
+			printf("Element found in pos %d\n", res);\
+	}
+
+	free(ptr);
+	return 0;
+}
--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ CC=gcc
 DIR1=bitops
 DIR2=leetcode
 DIR3=sorting
+DIR4=Algorithms
 
-all:fbitops fleetcode fsorting
+all:fbitops fleetcode fsorting fAlgo
 
 fbitops:
 	make -C $(DIR1)
@@ -11,9 +12,12 @@ fleetcode:
 	make -C $(DIR2)
 fsorting:
 	make -C $(DIR3)
+fAlgo:
+	make -C $(DIR4)
 
 .PHONY:
 clean:
 	make -C $(DIR1) clean
 	make -C $(DIR2) clean
 	make -C $(DIR3) clean
+	make -C $(DIR4) clean
